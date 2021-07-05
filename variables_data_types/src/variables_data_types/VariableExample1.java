4e458404fbaa9b1b57929340e243c49c99234762
krishna <vamshikrishna.amandu@gmail.com>
2021-07-05 23:34:24 +0530

    Variables and Data Types.

---
+++ variables_data_types/src/variables_data_types/VariableExample1.java
@@ -0,0 +1,31 @@
+package variables_data_types;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VariableExample1 {
+	
+	
+	byte size=32;
+	short age=150;
+	int numberOfSixes;
+	long distance=9000000;
+	float height=6.6f;
+	double salary =10000.95d;
+	
+	List<String> fruitNames = new ArrayList<String>();
+	String[] colours ={"Red","Blue"};
+	
+	
+//Here size ,age,numberOfSixes,height,salary are variables.
+	
+// byte ,short , int , long , float double are primitive Data types;
+	
+// 	fruitNames and colours are non primitive data types.
+	
+	
+	
+	
+
+	
+}
