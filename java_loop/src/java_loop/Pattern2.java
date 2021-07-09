0132a760ee29c2e9961b4dca3c81c3e719e45e95
krishna <vamshikrishna.amandu@gmail.com>
2021-07-09 18:53:34 +0530

    Pattern logic.

---
+++ java_loop/src/java_loop/Pattern2.java
@@ -0,0 +1,13 @@
+package java_loop;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 7; i++) {
+			System.out.println("");
+			for (int j = 7; i <= j; j--) {
+				System.out.print("*");
+			}
+		}
+
+	}
+}
---
+++ java_loop/src/java_loop/Pattern3.java
@@ -0,0 +1,16 @@
+package java_loop;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=5;i++) {
+			System.out.println(" ");
+			for (int j=0;j<=i;j++) {
+				System.out.print("*");
+			}
+		}
+
+	}
+
+}
